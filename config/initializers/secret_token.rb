7be8cf08598b6b34fe57045952f25c30510708a1
sunnyera <sunny.era@gmail.com>
2013-11-01 16:32:55 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emailer::Application.config.secret_token = '2774243f7dd00a194025898c44a0984395a13b43f027e976df76138640106e1d6e0312246ea2dec5f8d6efe5bd083e1dad88e784c6fb436df3314b72f3085d90'
